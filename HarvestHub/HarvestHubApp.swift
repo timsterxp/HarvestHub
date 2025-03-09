8b19c356be1c6ccd091e76acba01b3c58d4ec795
Timothy Nguyen <timmyn97@gmail.com>
2025-03-09 11:51:05 -0700

    Initial Commit

---
+++ HarvestHub/HarvestHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarvestHubApp.swift
+//  HarvestHub
+//
+//  Created by Spartan on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct HarvestHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HarvestHubTests/HarvestHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  HarvestHubTests.swift
+//  HarvestHubTests
+//
+//  Created by Spartan on 3/9/25.
+//
+
+import Testing
+@testable import HarvestHub
+
+struct HarvestHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
